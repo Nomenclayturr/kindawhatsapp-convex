02656a71ca99358205626c635bd4467bb53ab667
Nomenclayturr <limpbuiscuit@gmail.com>
2025-03-20 16:28:43 -0500

    backend files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ConvexClient } from "convex/browser";
+import { ConvexClientProvider } from "@/components/ui/providers/convex-provider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +26,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+   <ClerkProvider dynamic>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ConvexClientProvider>
         {children}
+        </ConvexClientProvider>
       </body>
     </html>
+  </ClerkProvider>
   );
 }
---
+++ convex/schema.ts
@@ -0,0 +1,18 @@
+import { profile } from "console";
+import { defineSchema, defineTable } from "convex/server";
+import { v} from "convex/values";
+
+export default defineSchema({
+    users: defineTable({
+        userId: v.string(),
+        email: v.string(),
+        createdAt: v.number(),
+        
+        name: v.optional(v.string()),
+        profileImage: v.optional(v.string()),
+    })
+    .index("by_userId", ["userId"])
+    .index("by_name", ["name"])
+    .index("by_email", ["email"])
+
+    })
