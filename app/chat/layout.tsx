ab3322229ce4244211c3498363cfc90a7927b807
Nomenclayturr <limpbuiscuit@gmail.com>
2025-03-21 11:33:04 -0500

    chats and schema for convex

---
+++ app/chat/layout.tsx
@@ -0,0 +1,32 @@
+import { auth } from "@clerk/nextjs/server"
+import { api } from "@/convex/_generated/api"
+import { preloadQuery } from "convex/nextjs"
+import ChatLayoutWrapper from "./_components/chat-layout-wrapper"
+
+
+
+export default async function ChatLayout({ children }: {
+    children: React.ReactNode
+  }) {
+    const { userId } = await auth()
+  
+    // user information
+    const preloadedUserInfo = await preloadQuery(api.users.readUser, {
+      userId: userId!
+    })
+    // conversations + chats
+    const preloadedConversations = await preloadQuery(api.chats.getConversation, {
+      userId: userId!
+    })
+  
+    // preloaded chat
+  
+    return (
+      <ChatLayoutWrapper
+        preloadedUserInfo={preloadedUserInfo}
+        preloadedConversations={preloadedConversations}
+      >
+        {children}
+      </ChatLayoutWrapper>
+    )
+  }
